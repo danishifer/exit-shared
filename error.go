166f3f25d1fb1162f748afb8f5a4e7ff62df7291
Dani Shifer <dani.shifer@gmail.com>
2020-01-13 19:14:37 +0200

    :construction: Transition to Go Modules

--- error.go
+++ error.go
@@ -1,1 +1,12 @@
 package exit_shared
+
+import "fmt"
+
+type FrameworkHTTPError struct {
+	Code    int
+	Message string
+}
+
+func (e *FrameworkHTTPError) Error() string {
+	return fmt.Sprintf("framework error [%d]: \"%s\"", e.Code, e.Message)
+}
--- institute-service/get_terminal_by_card_serial.go
+++ institute-service/get_terminal_by_card_serial.go
@@ -1,1 +1,18 @@
 package instituteService
+
+import exit "exit-shared"
+
+type GetTerminalByCardSerialRequest struct {
+	CardSerial string `json:"card_serial"`
+}
+
+type GetTerminalByCardSerialResponse struct {
+	Terminal exit.Terminal `json:"terminal"`
+}
+
+func (r *GetTerminalByCardSerialRequest) IsValid() bool {
+	if r.CardSerial == "" {
+		return false
+	}
+	return true
+}
